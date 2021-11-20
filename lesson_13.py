93160e6841d209b62fd746608091342b8f099686
Nastya Rbh <nastyarbh1609@gmail.com>
2021-11-20 21:33:15 +0200

    add lesson 13

---
+++ lesson_13.py
@@ -0,0 +1,24 @@
+def add(arg):
+    print("Создаём запись")
+
+    return
+
+def find(arg):
+    print("Ищем запись")
+
+    return
+
+def edit(arg):
+    print("Редактируем запись")
+
+    return
+
+def delete(arg):
+    print("Удаляем запись")
+
+    return
+
+def exit(arg):
+    print("Выходим из программы")
+
+    return
--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import lesson_12 as mm
 
 print(mm.rus_facults('gryffindor'))
 
+
+
+import lesson_13 as menu
+
+menu.add()
+
